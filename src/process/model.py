a62469ec28158f06a2737b1f0d7c9797222680e5
polowitty <yangzhep@qq.com>
2023-10-17 13:33:51 +0000

    conv layer as readout layer

--- src/process/model.py
+++ src/process/model.py
@@ -25,6 +25,53 @@ def init_weights(m):
     if type(m) == nn.Linear or type(m) == nn.Conv1d:
         torch.nn.init.xavier_uniform_(m.weight)
 
+class Conv(nn.Module):
+
+    def __init__(self, conv1d_1, conv1d_2, maxpool1d_1, maxpool1d_2, fc_1_size, fc_2_size):
+        super(Conv, self).__init__()
+        self.conv1d_1_args = conv1d_1
+        self.conv1d_1 = nn.Conv1d(**conv1d_1)
+        self.conv1d_2 = nn.Conv1d(**conv1d_2)
+
+        fc1_size = get_conv_mp_out_size(fc_1_size, conv1d_2, [maxpool1d_1, maxpool1d_2])
+        fc2_size = get_conv_mp_out_size(fc_2_size, conv1d_2, [maxpool1d_1, maxpool1d_2])
+
+        # Dense layers
+        self.fc1 = nn.Linear(fc1_size, 1)
+        self.fc2 = nn.Linear(fc2_size, 1)
+
+        # Dropout
+        self.drop = nn.Dropout(p=0.2)
+
+        self.mp_1 = nn.MaxPool1d(**maxpool1d_1)
+        self.mp_2 = nn.MaxPool1d(**maxpool1d_2)
+
+    def forward(self, hidden, x):
+        concat = torch.cat([hidden, x], 1)
+        concat_size = hidden.shape[1] + x.shape[1]
+        concat = concat.view(-1, self.conv1d_1_args["in_channels"], concat_size)
+
+        Z = self.mp_1(F.relu(self.conv1d_1(concat)))
+        Z = self.mp_2(self.conv1d_2(Z))
+
+        hidden = hidden.view(-1, self.conv1d_1_args["in_channels"], hidden.shape[1])
+
+        Y = self.mp_1(F.relu(self.conv1d_1(hidden)))
+        Y = self.mp_2(self.conv1d_2(Y))
+
+        Z_flatten_size = int(Z.shape[1] * Z.shape[-1])
+        Y_flatten_size = int(Y.shape[1] * Y.shape[-1])
+
+        Z = Z.view(-1, Z_flatten_size)
+        Y = Y.view(-1, Y_flatten_size)
+        res = self.fc1(Z) * self.fc2(Y)
+        res = self.drop(res)
+        # res = res.mean(1)
+        # print(res, mean)
+        sig = torch.sigmoid(torch.flatten(res))
+        return sig
+
+
 class Readout(nn.Module):
     def __init__(self, emb_size, hidden_dim):
         super(Readout, self).__init__()
@@ -47,16 +94,20 @@ class Readout(nn.Module):
         return global_feat
 
 class Net(nn.Module):
-    def __init__(self, gated_graph_conv_args, emb_size, max_nodes, device):
+    def __init__(self, gated_graph_conv_args, conv_args, emb_size, max_nodes, device):
         super(Net, self).__init__()
         self.ggc = GatedGraphConv(**gated_graph_conv_args).to(device) 
         self.emb_size=emb_size
         self.readout = Readout(emb_size,gated_graph_conv_args['out_channels']).to(device) 
-
+        self.conv = Conv(**conv_args,
+                    fc_1_size=gated_graph_conv_args["out_channels"] + emb_size,
+                    fc_2_size=gated_graph_conv_args["out_channels"]).to(device)
+        
     def forward(self, data):
         x, edge_index = data.x, data.edge_index
         x = self.ggc(x, edge_index)
         x = self.readout(x, data.x, data.batch)
+        # x = self.conv(x, data.x)
 
         return x
 
